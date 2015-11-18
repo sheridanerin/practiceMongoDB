7ae3aac1379e8fdb36a7030ffc7f49d355c6ce32
Erin Sheridan <SheridanErin@gmail.com>
2015-11-18 11:28:54 -0700

    server.js updates

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express')
     , bodyParser = require('body-parser')
     , cors = require('cors')
     , mongojs = require('mongojs')
-    , bird = require('./birdsModel.js');
+    , Bird = require('./birdsModel.js');
     , port = 8000
     , db = mongojs('birds')
     , sighting = db.collection('sighting');
@@ -11,6 +11,56 @@ var app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+
+Bird.find({}, function( err, data ) {
+	if (err) res.status(500).send(err);
+	else res.send(data);
+})
+
+bird.find() //1st way to do this!!!!!!
+	.where('wingspan').gt(12) // allows all wingspans greater than 12.
+	.where('color').equals('blood red') // allows only birds with property color equal to red. 
+	.sort(-"wingspan") // the dash means descending(largest to smallest).  With no dash it is ascending(smallest to largest)
+	.limit(10);  // allows us to say we only want the first 10.
+	.exec(function( err, data ) {
+		if (err) res.status(500).send(err);
+		else res.send(data);
+	})
+
+bird.find() //2nd way to do this!!!!!!
+	.where('wingspan').gt(12) // allows all wingspans greater than 12.
+	.where('color').equals('blood red') // allows only birds with property color equal to red. 
+	.sort(-"wingspan") // the dash means descending(largest to smallest).  With no dash it is ascending(smallest to largest)
+	.limit(10);  // allows us to say we only want the first 10.
+	.exec() // this automatically returns a promise which means we can use a .then().
+	.then(function( data ) {
+		//Handles results
+	} function( err ) {
+		//Hanldes err
+	})
+
+
+var newBird = new Bird({ // creates a new bird model, this would most like be passed new info from the view. 
+		scientificName: "Genus Menus Birdus Maxius"
+	,	color: "blood red"
+	, 	region: "Sparta"
+	,	firstSightingEver: new date();
+	,	food: ["people", "bugs"]
+	, 	wingspan: 17
+	, 	endangered: false
+	,	nest: {
+			materials: ["blown Over Trees", "rooftops", "people"]
+		,	size: 500
+		,	timeToBuild: 200
+		,	locationDesc: "mountaintops"
+		}
+	//etc
+});
+
+newBird.save(function( err, data ) { // pushes new bird to database
+	if (err) res.status(500).send(err);
+	else res.send(data);
+})
 
 app.post('/api/sighting', function(req, res, next) {
 	sighting.insert(req.body, function( err, data ) {
